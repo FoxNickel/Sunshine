fcefe0ad27d0f018a72054bb21e98c1275a694c4
NickelFox <49366499@qq.com>
2017-01-21 13:26:14 +0800

    改用UriBuilder构建url

--- app/src/main/java/cn/foxnickel/sunshine/FetchWeatherTask.java
+++ app/src/main/java/cn/foxnickel/sunshine/FetchWeatherTask.java
@@ -1,5 +1,6 @@
 package cn.foxnickel.sunshine;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -14,15 +15,43 @@ import java.net.URL;
  * Created by Administrator on 2017/1/20.
  */
 
-public class FetchWeatherTask extends AsyncTask {
+public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
+
+    private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
     @Override
-    protected Void doInBackground(Object[] params) {
+    protected Void doInBackground(String... params) {
+
+        if (params.length == 0) {
+            return null;
+        }
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
-        String forecastJsonStr = null;
+        String forecastJsonStr = null;//查询得到的字符串
+        /*查询数据需要的参数*/
+        String format = "json";
+        String units = "metric";
+        int numDays = 7;
+        String appid = "97056b3b38944b922083c074a9e119da";
         try {
-            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&APPID=97056b3b38944b922083c074a9e119da");
+            /*构建uri时需要用到的参数*/
+            final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+            final String QUERY_PARAM = "q";
+            final String FORMAT_PARAM = "mode";
+            final String UNITS_PARAM = "units";
+            final String DAYS_PARAM = "cnt";
+            final String APPID_PARAM = "APPID";
+            /*构建Uri*/
+            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, params[0])
+                    .appendQueryParameter(FORMAT_PARAM, format)
+                    .appendQueryParameter(UNITS_PARAM, units)
+                    .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                    .appendQueryParameter(APPID_PARAM, appid)
+                    .build();
+            URL url = new URL(builtUri.toString());
+            Log.i(LOG_TAG, "Built URI " + builtUri.toString());
+            /*开始Http请求*/
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
@@ -43,7 +72,7 @@ public class FetchWeatherTask extends AsyncTask {
                 forecastJsonStr = null;
             }
             forecastJsonStr = buffer.toString();
-            Log.i("FetchWeatherTask", forecastJsonStr);
+            Log.i(LOG_TAG, forecastJsonStr);
         } catch (IOException e) {
             Log.e("ForecastFragment", "Error ", e);
             forecastJsonStr = null;
