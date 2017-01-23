d2309709854acc78ed6965676367ab223e350a3c
NickelFox <49366499@qq.com>
2017-01-23 15:43:39 +0800

    添加了listview项的点击事件，点击之后会Toast当前项的text内容

--- app/src/main/java/cn/foxnickel/sunshine/ForecastFragment.java
+++ app/src/main/java/cn/foxnickel/sunshine/ForecastFragment.java
@@ -14,8 +14,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -79,6 +81,12 @@ public class ForecastFragment extends Fragment {
                 weekForecast);
         ListView weatherList = (ListView) rootView.findViewById(R.id.listview_forecast);
         weatherList.setAdapter(adapter);
+        weatherList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), adapter.getItem(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return rootView;
     }
 
