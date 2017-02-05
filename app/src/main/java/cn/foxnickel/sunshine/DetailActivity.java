ad360d18052e3ef9cbb70721e40ff9041714d939
NickelFox <49366499@qq.com>
2017-02-05 11:09:22 +0800

    无效的修改

--- app/src/main/java/cn/foxnickel/sunshine/DetailActivity.java
+++ app/src/main/java/cn/foxnickel/sunshine/DetailActivity.java
@@ -3,12 +3,14 @@ package cn.foxnickel.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private ShareActionProvider provider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem item = menu.findItem(R.id.action_share);
         return true;
     }
 
