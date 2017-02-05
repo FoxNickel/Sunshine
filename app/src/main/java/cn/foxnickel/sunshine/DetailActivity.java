4e0bc9f65b87fa9f038afb8a1f19c8703fa9eb93
NickelFox <49366499@qq.com>
2017-02-05 10:57:19 +0800

    添加了ShareIntent，可以在DetailActivity里面将今天的天气情况进行分享，（使用的是support库里面的东西）

--- app/src/main/java/cn/foxnickel/sunshine/DetailActivity.java
+++ app/src/main/java/cn/foxnickel/sunshine/DetailActivity.java
@@ -2,26 +2,54 @@ package cn.foxnickel.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private ShareActionProvider mProvider;
+    private String forecastStr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         TextView detailForecast = (TextView) findViewById(R.id.detail_forecast);
-        Intent forecast = getIntent();
-        detailForecast.setText(forecast.getStringExtra(Intent.EXTRA_TEXT));
+        Intent forecastIntent = getIntent();
+        forecastStr = forecastIntent.getStringExtra(Intent.EXTRA_TEXT);
+        /*判断Intent里有数据的时候再更新界面*/
+        if (forecastIntent != null && forecastIntent.hasExtra(Intent.EXTRA_TEXT)) {
+            detailForecast.setText(forecastStr);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail, menu);
+        // 用ShareActionProvider定位MenuItem
+        MenuItem item = menu.findItem(R.id.action_share);
+        // 获取并存储ShareActionProvider
+        mProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        setShareIntent();
         return true;
+    }
+
+    private void setShareIntent() {
+        if (mProvider != null) {
+            mProvider.setShareIntent(createShareForecastIntent());
+        }
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                forecastStr + "#SunshineApp");
+        return shareIntent;
     }
 
     @Override
